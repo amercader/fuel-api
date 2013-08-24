640bf631a055dbcb0f70741a95a479bc080656c8
amercader <amercadero@gmail.com>
2013-08-24 19:36:55 +0100

    Add autocomplete API method
    It uses two edge n-gram fields in the Solr schema

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ var util = require('util');
 var solr = require('solr-client');
 
 var versions = ['1'];
+var _debug = false;
 
 exports.checkVersion = function(req, res, next) {
   if (versions.indexOf(req.params.version) == -1) {
@@ -11,11 +12,15 @@ exports.checkVersion = function(req, res, next) {
   next();
 }
 
+exports.debug = function(req, res, next) {
+  _debug = Boolean(req.query.debug);
+  next();
+}
 
 exports.search= function(req, res) {
 
   var solrClient = solr.createClient('localhost', '8080', 'fuel');
-  var query = solrClient.createQuery()
+  var query = solr.createClient().createQuery();
   var debug = Boolean(req.query['debug']);
   var parts;
 
@@ -27,8 +32,8 @@ exports.search= function(req, res) {
     }
   }
 
-  if (req.query['sort']) {
-    var fields = req.query['sort'].split(',');
+  if (req.query.sort) {
+    var fields = req.query.sort.split(',');
     var sort = {};
     for (var i = 0; i < fields.length; i++) {
       parts = fields[i].trim().split(' ');
@@ -37,25 +42,8 @@ exports.search= function(req, res) {
     query.sort(sort);
   }
 
-  if (req.query['fq']) {
-    var fq = req.query['fq'];
-    var removeBrackets = /\[|\]/g;
-    var range;
-    if (!util.isArray(req.query['fq'])) {
-      fq = [fq];
-    }
-    for (var i = 0; i < fq.length; i++) {
-      parts = fq[i].trim().split(':');
-      if (parts[1].indexOf('[') !== -1) {
-        range = parts[1].trim().replace(removeBrackets,'').split(' TO ');
-        if (range.length !== 2) {
-          res.send('400', {error: {type: 'api', msg: 'Wrong fq parameter:' + fq[i]}});
-        }
-        query.rangeFilter({ 'field': parts[0].trim(), 'start': range[0], 'end': range[1]});
-      } else {
-        query.matchFilter(parts[0].trim(), parts[1].trim());
-      }
-    }
+  if (req.query.fq) {
+    addFilters(query, req.query.fq);
   }
 
   var facetOptions = {}
@@ -106,7 +94,7 @@ exports.search= function(req, res) {
         resObj.results.push(obj.response.docs[i]);
       }
 
-      if (debug) {
+      if (_debug) {
         resObj['__debug'] = {
           'queryParams': req.query,
           'solrResponse': obj['responseHeader']
@@ -118,3 +106,90 @@ exports.search= function(req, res) {
   });
 }
 
+exports.autocomplete_fields = ['manufacturer', 'model'];
+
+exports.autocomplete = function(req, res) {
+
+  if ((exports.autocomplete_fields).indexOf(req.params.field) === -1) {
+    res.send('404', {error: {type: 'api', msg: 'Supported autocomplete fields are: ' + exports.autocomplete_fields}});
+  }
+
+  var solrClient = solr.createClient('localhost', '8080', 'fuel');
+  var query = solrClient.createQuery()
+  var debug = Boolean(req.query['debug']);
+
+  var q = (req.query.q) ? '"' + req.query.q + '"' : '*';
+  query.q(req.params.field + '_suggest:' + q);
+
+  query.rows(0);
+
+  if (req.query.fq) {
+    addFilters(query, req.query.fq);
+  }
+
+  query.facet({
+    on: true,
+    mincount: 1,
+    sort: req.query.sort || 'index',
+    field: req.params.field + '_s'
+  });
+
+  solrClient.search(query, function(err, obj){
+    if (err) {
+      var solrError = err.message.match(/{(.*)}/g);
+      if (solrError) {
+        solrError = JSON.parse(solrError);
+        res.send(solrError.error.code, {error: {type: 'solr', 'msg': solrError.error.msg}});
+      } else {
+        res.send(500, {error: {type: 'solr', 'msg': 'Unknown Solr error'}});
+      }
+
+    } else {
+
+      var resObj = {
+        field: req.params.field,
+        results: []
+      }
+
+      var results = obj.facet_counts.facet_fields[req.params.field + '_s'];
+      for (var i = 0; i < results.length; i = i + 2) {
+        resObj.results.push({value: results[i], count: results[i + 1]});
+      }
+
+
+
+      if (debug) {
+        resObj['__debug'] = {
+          'queryParams': req.query,
+          'solrResponse': obj['responseHeader']
+        };
+      }
+
+      res.send(resObj);
+    }
+  });
+
+}
+
+var removeBrackets = /\[|\]/g;
+function addFilters(query, fq) {
+  var range;
+  if (!util.isArray(fq)) {
+    fq = [fq];
+  }
+  for (var i = 0; i < fq.length; i++) {
+    if (fq[i].indexOf(':') === -1) {
+      continue;
+    }
+    parts = fq[i].trim().split(':');
+    if (parts[1].indexOf('[') !== -1) {
+      range = parts[1].trim().replace(removeBrackets,'').split(' TO ');
+      if (range.length !== 2) {
+        res.send('400', {error: {type: 'api', msg: 'Wrong fq parameter:' + fq[i]}});
+      }
+      query.rangeFilter({ 'field': parts[0].trim(), 'start': range[0], 'end': range[1]});
+    } else {
+      query.matchFilter(parts[0].trim(), parts[1].trim());
+    }
+  }
+}
--- server.js
+++ server.js
@@ -8,13 +8,26 @@ app.get('/api', function(req, res){
   res.send({versions: [1]});
 });
 
-var apiMiddleware = [api.checkVersion];
+var apiMiddleware = [api.checkVersion, api.debug];
 
 app.get('/api/:version', apiMiddleware, function(req, res){
-  res.send({methods: ['search']});
+  res.send({methods: ['search', 'autocomplete']});
 });
 
 app.get('/api/:version/search', apiMiddleware, api.search);
+
+
+app.get('/api/:version/autocomplete', apiMiddleware, function(req, res){
+  res.send({fields: ['manufacturer', 'model']});
+});
+
+app.get('/api/:version/autocomplete/:field', apiMiddleware, api.autocomplete);
+
+app.get('/browse', function(req, res){
+  res.sendfile(__dirname + '/public/browse.html');
+});
+
+app.use(express.static('public'));
 
 app.listen(3000);
 console.log('Listening to port 3000');
